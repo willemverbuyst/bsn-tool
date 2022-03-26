08ff7e2e8c6589bc980bd40429d2292a3f16c3e2
Willem Verbuyst <willemverbuyst@gmail.com>
2022-03-26 10:01:41 +0100

    Write simple test

---
+++ cypress/integration/tests/TC001.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('page bsn tool', () => {
+	before(() => {
+		cy.visit('/')
+	})
+
+	it('displays title BSN-tool', () => {
+		cy.get('.title').contains('BSN-tool')
+	})
+
+	it('has a "generate BSN" button', () => {
+		cy.get('#bsn-generator-btn').contains('generate BSN')
+	})
+
+	it('has a "validate BSN" button', () => {
+		cy.get('#bsn-validator-btn').contains('validate BSN')
+	})
+
+	it('has an input field for bsn numbers', () => {
+		cy.get('#bsn-number')
+	})
+})
