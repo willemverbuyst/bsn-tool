f983880f52f4b4c792b385fd093f73d31d9d92b8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-24 06:11:43 +0100

    Upgrade Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 5000,
@@ -9,9 +9,10 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // eslint-disable-next-line global-require
+      return require("./cypress/plugins")(on, config);
     },
-    baseUrl: 'http://localhost:1234',
-    specPattern: 'cypress/e2e/**/*.spec.js',
+    baseUrl: "http://localhost:1234",
+    specPattern: "cypress/e2e/**/*.spec.js",
   },
-})
+});
--- cypress/e2e/tests/TC001.spec.js
+++ cypress/e2e/tests/TC001.spec.js
@@ -1,23 +1,23 @@
 /// <reference types="cypress" />
 
-describe('page bsn tool', () => {
-	before(() => {
-		cy.visit('/')
-	})
+describe("page bsn tool", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-	it('displays title BSN-tool', () => {
-		cy.get('.title').contains('BSN-tool')
-	})
+  it("displays title BSN-tool", () => {
+    cy.get(".title").contains("BSN-tool");
+  });
 
-	it('has a "generate BSN" button', () => {
-		cy.get('#bsn-generator-btn').contains('generate BSN')
-	})
+  it('has a "generate BSN" button', () => {
+    cy.get("#bsn-generator-btn").contains("generate BSN");
+  });
 
-	it('has a "validate BSN" button', () => {
-		cy.get('#bsn-validator-btn').contains('validate BSN')
-	})
+  it('has a "validate BSN" button', () => {
+    cy.get("#bsn-validator-btn").contains("validate BSN");
+  });
 
-	it('has an input field for bsn numbers', () => {
-		cy.get('#bsn-number')
-	})
-})
+  it("has an input field for bsn numbers", () => {
+    cy.get("#bsn-number");
+  });
+});
