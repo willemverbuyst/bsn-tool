97ff8ab42458f82d798d13e65d7271bbfa7efd2e
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-24 05:44:24 +0100

    Use default prettier config

--- src/script.js
+++ src/script.js
@@ -1,52 +1,52 @@
-import { generateBSN, isValidBSN } from 'bsn-js'
+import { generateBSN, isValidBSN } from "bsn-js";
 
 const BSNtool = () => {
-	const bsnNumber = document.getElementById('bsn-number')
-	const feedback = document.getElementById('feedback')
-	const bsnGeneratorBtn = document.getElementById('bsn-generator-btn')
-	const bsnValidatorBtn = document.getElementById('bsn-validator-btn')
+  const bsnNumber = document.getElementById("bsn-number");
+  const feedback = document.getElementById("feedback");
+  const bsnGeneratorBtn = document.getElementById("bsn-generator-btn");
+  const bsnValidatorBtn = document.getElementById("bsn-validator-btn");
 
-	const validateBSN = inputValue => {
-		const isValid = isValidBSN(inputValue)
+  const validateBSN = (inputValue) => {
+    const isValid = isValidBSN(inputValue);
 
-		if (inputValue !== '' && !isValid) {
-			feedback.textContent = 'this bsn is not valid'
-			feedback.style.opacity = '100'
-			feedback.style.color = 'red'
-		} else if (inputValue !== '' && isValid) {
-			feedback.textContent = 'this is a valid bsn'
-			feedback.style.opacity = '100'
-			feedback.style.color = 'green'
-		} else {
-			feedback.textContent = 'placeholder'
-			feedback.style.opacity = '0'
-		}
-	}
+    if (inputValue !== "" && !isValid) {
+      feedback.textContent = "this bsn is not valid";
+      feedback.style.opacity = "100";
+      feedback.style.color = "red";
+    } else if (inputValue !== "" && isValid) {
+      feedback.textContent = "this is a valid bsn";
+      feedback.style.opacity = "100";
+      feedback.style.color = "green";
+    } else {
+      feedback.textContent = "placeholder";
+      feedback.style.opacity = "0";
+    }
+  };
 
-	bsnGeneratorBtn.addEventListener('click', () => {
-		const newBSN = generateBSN()
+  bsnGeneratorBtn.addEventListener("click", () => {
+    const newBSN = generateBSN();
 
-		feedback.textContent = 'placeholder'
-		feedback.style.opacity = '0'
-		bsnNumber.value = newBSN
-	})
+    feedback.textContent = "placeholder";
+    feedback.style.opacity = "0";
+    bsnNumber.value = newBSN;
+  });
 
-	bsnValidatorBtn.addEventListener('click', () => {
-		const inputValue = bsnNumber.value.trim()
-		validateBSN(inputValue)
-	})
+  bsnValidatorBtn.addEventListener("click", () => {
+    const inputValue = bsnNumber.value.trim();
+    validateBSN(inputValue);
+  });
 
-	bsnNumber.addEventListener('input', () => {
-		feedback.textContent = 'placeholder'
-		feedback.style.opacity = '0'
-	})
+  bsnNumber.addEventListener("input", () => {
+    feedback.textContent = "placeholder";
+    feedback.style.opacity = "0";
+  });
 
-	bsnNumber.addEventListener('keypress', e => {
-		if (e.key === 'Enter') {
-			const inputValue = bsnNumber.value.trim()
-			validateBSN(inputValue)
-		}
-	})
-}
+  bsnNumber.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const inputValue = bsnNumber.value.trim();
+      validateBSN(inputValue);
+    }
+  });
+};
 
-BSNtool()
+BSNtool();
