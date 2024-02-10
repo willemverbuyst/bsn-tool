c0742bb603be6a3d86d5bc61180f7bac0e43cf75
Willem Verbuyst <willemverbuyst@gmail.com>
2024-02-10 06:35:30 +0100

    Clean up cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,11 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   defaultCommandTimeout: 5000,
   pageLoadTimeout: 10000,
   video: false,
   waitForAnimations: true,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line global-require
-      return require("./cypress/plugins")(on, config);
-    },
     baseUrl: "http://localhost:1234",
     specPattern: "cypress/e2e/**/*.spec.js",
   },
