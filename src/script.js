f40fdd1f1ffdaaaf12f154c5152beb594d9d3e1a
Willem Verbuyst <willemverbuyst@gmail.com>
2024-02-10 05:57:54 +0100

    Make copy btn conditional

--- src/script.js
+++ src/script.js
@@ -6,23 +6,28 @@ const BSNtool = () => {
   const bsnGeneratorBtn = document.getElementById("bsn-generator-btn");
   const bsnValidatorBtn = document.getElementById("bsn-validator-btn");
   const copyButton = document.getElementById("bsn-number__copy-button");
+  const copyIcon = document.getElementById("bsn-number__copy-icon");
 
-  const validateBSN = (inputValue) => {
-    const isValid = isValidBSN(inputValue);
-
+  function updateStylesAfterValidation(inputValue, isValid) {
     if (inputValue !== "" && !isValid) {
       feedback.textContent = "this bsn is not valid";
       feedback.style.opacity = "100";
       feedback.style.color = "red";
+      copyIcon.classList.replace("fa-check", "fa-copy");
+      copyButton.style.display = "none";
     } else if (inputValue !== "" && isValid) {
       feedback.textContent = "this is a valid bsn";
       feedback.style.opacity = "100";
       feedback.style.color = "green";
+      copyIcon.classList.replace("fa-check", "fa-copy");
+      copyButton.style.display = "block";
     } else {
       feedback.textContent = "placeholder";
       feedback.style.opacity = "0";
+      copyIcon.classList.replace("fa-check", "fa-copy");
+      copyButton.style.display = "none";
     }
-  };
+  }
 
   bsnGeneratorBtn.addEventListener("click", () => {
     const newBSN = generateBSN();
@@ -30,26 +35,32 @@ const BSNtool = () => {
     feedback.textContent = "placeholder";
     feedback.style.opacity = "0";
     bsnNumber.value = newBSN;
+    copyIcon.classList.replace("fa-check", "fa-copy");
+    copyButton.style.display = "block";
   });
 
   bsnValidatorBtn.addEventListener("click", () => {
     const inputValue = bsnNumber.value.trim();
-    validateBSN(inputValue);
+    const isValid = isValidBSN(inputValue);
+    updateStylesAfterValidation(inputValue, isValid);
   });
 
   bsnNumber.addEventListener("input", () => {
     feedback.textContent = "placeholder";
     feedback.style.opacity = "0";
+    copyButton.style.display = "none";
   });
 
   bsnNumber.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
       const inputValue = bsnNumber.value.trim();
-      validateBSN(inputValue);
+      const isValid = isValidBSN(inputValue);
+      updateStylesAfterValidation(inputValue, isValid);
     }
   });
 
   copyButton.addEventListener("click", () => {
+    copyIcon.classList.replace("fa-copy", "fa-check");
     navigator.clipboard.writeText(bsnNumber.value);
   });
 };
