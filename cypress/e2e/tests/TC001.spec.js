7e861676891c0dbbf346a04d8507dca1694d8c49
Willem Verbuyst <willemverbuyst@gmail.com>
2024-02-10 07:03:25 +0100

    Update cypress tests

--- cypress/e2e/tests/TC001.spec.js
+++ cypress/e2e/tests/TC001.spec.js
@@ -20,4 +20,33 @@ describe("page bsn tool", () => {
   it("has an input field for bsn numbers", () => {
     cy.get("#bsn-number");
   });
+
+  it("should not display copy button when there is no input", () => {
+    cy.get("#bsn-number__copy-button").should("not.be.visible");
+  });
+
+  it("should display copy button when a valid bsn has been generated", () => {
+    cy.get("#bsn-generator-btn").click();
+    cy.get("#bsn-number__copy-button").should("be.visible");
+  });
+
+  it("should display copy icon when copy button is visible", () => {
+    cy.get("#bsn-generator-btn").click();
+    cy.get("#bsn-number__copy-icon").should("have.class", "fa fa-copy");
+  });
+
+  it("should display checkmark icon when copy button has been clicked", () => {
+    cy.get("#bsn-number").type("999999023");
+    cy.get("#bsn-validator-btn").click();
+    cy.get("#bsn-number__copy-button").realClick();
+    cy.get("#bsn-number__copy-icon").should("have.class", "fa fa-check");
+  });
+
+  it("should hide copy button when input has changed", () => {
+    cy.get("#bsn-number").type("999999023");
+    cy.get("#bsn-validator-btn").click();
+    cy.get("#bsn-number__copy-button").should("exist");
+    cy.get("#bsn-number").type("555");
+    cy.get("#bsn-number__copy-button").should("not.be.visible");
+  });
 });
