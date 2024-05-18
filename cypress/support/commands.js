de2a4909623b22a345958e2ed8b768c48f833663
Willem Verbuyst <willemverbuyst@gmail.com>
2024-05-18 05:32:16 +0200

    Update eslint configuration

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,5 +23,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import "cypress-real-events";
