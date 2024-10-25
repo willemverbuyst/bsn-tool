e8793f9f30ad6a9a13291c9c6dc6147f20caff8b
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-10-25 04:25:33 +0200

    Write tests for CopyButton

---
+++ src/CopyButton.spec.ts
@@ -0,0 +1,73 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import CopyButton from "./CopyButton";
+
+describe("CopyButton", () => {
+  let copyButton: CopyButton;
+  let button: HTMLButtonElement;
+  let copyIcon: HTMLSpanElement;
+  let bsnNumber: HTMLInputElement;
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div>
+        <input
+          type="text"
+          name="bsn-number"
+          id="bsn-number"
+          aria-label="bsn-number"
+        />
+        <button id="bsn-number__copy-button" type="button">
+          <i class="fa fa-copy" id="bsn-number__copy-icon"></i>
+        </button>
+      </div>
+    `;
+
+    button = <HTMLButtonElement>(
+      document.getElementById("bsn-number__copy-button")
+    );
+    copyIcon = <HTMLSpanElement>(
+      document.getElementById("bsn-number__copy-icon")
+    );
+    bsnNumber = <HTMLInputElement>document.getElementById("bsn-number");
+
+    copyButton = new CopyButton();
+  });
+
+  it("should hide the button and set the correct icon class", () => {
+    copyButton.hide();
+    expect(copyIcon.classList.contains("fa-check")).toBe(false);
+    expect(copyIcon.classList.contains("fa-copy")).toBe(true);
+    expect(button.style.display).toBe("none");
+  });
+
+  it("should display the button and set the correct icon class", () => {
+    copyButton.display();
+    expect(copyIcon.classList.contains("fa-check")).toBe(false);
+    expect(copyIcon.classList.contains("fa-copy")).toBe(true);
+    expect(button.style.display).toBe("block");
+  });
+
+  it("should display the button and set the check mark icon class", () => {
+    copyButton.displayCheckMark();
+    expect(copyIcon.classList.contains("fa-copy")).toBe(false);
+    expect(copyIcon.classList.contains("fa-check")).toBe(true);
+    expect(button.style.display).toBe("block");
+  });
+
+  it("should set up the click event listener and copy text to clipboard", () => {
+    const writeTextMock = vi.fn();
+    vi.spyOn(navigator.clipboard, "writeText").mockImplementation(
+      writeTextMock,
+    );
+
+    bsnNumber.value = "123456789";
+
+    copyButton.listen(bsnNumber);
+    button.click();
+
+    expect(writeTextMock).toHaveBeenCalledWith("123456789");
+    expect(copyIcon.classList.contains("fa-copy")).toBe(false);
+    expect(copyIcon.classList.contains("fa-check")).toBe(true);
+    expect(button.style.display).toBe("block");
+  });
+});
