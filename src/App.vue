7368b8c6a5db38659f7571d5b27de5a70c8502f9
Willem Verbuyst <willemverbuyst@gmail.com>
2025-10-01 06:07:19 +0200

    Copy over html and css from legacy code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <section class="link">
+    <a
+      href="https://github.com/willemverbuyst/bsn-tool"
+      target="_blank"
+      rel="noreferrer"
+      aria-labelledby="gh-icon"
+      ><i class="fab fa-github icon__github">GH ICON</i></a
+    >
+  </section>
+
+  <header>
+    <h1>BSN-tool</h1>
+  </header>
+
+  <main>
+    <form autocomplete="off" onsubmit="return false;">
+      <section>
+        <button id="bsn-generator-btn" type="button">generate BSN</button>
+      </section>
+      <section>
+        <div>
+          <input
+            type="text"
+            name="bsn-number"
+            id="bsn-number"
+            aria-label="bsn-number"
+          />
+          <button id="bsn-number__copy-button" type="button">
+            <i class="fa fa-copy" id="bsn-number__copy-icon">COPY</i>
+          </button>
+        </div>
+        <p id="feedback"></p>
+      </section>
+      <section>
+        <button id="bsn-validator-btn" type="button">validate BSN</button>
+      </section>
+    </form>
+  </main>
 </template>
 
 <style scoped></style>
