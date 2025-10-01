d15c70500709bf44167608ddd826df7a3462f79c
Willem Verbuyst <willemverbuyst@gmail.com>
2025-10-01 07:01:17 +0200

    Separate Header

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,12 @@ import BsnGeneratorButton from "./components/BsnGeneratorButton.vue";
 import BsnInput from "./components/BsnInput.vue";
 import BsnValidatorButton from "./components/BsnValidatorButton.vue";
 import GithubLink from "./components/GithubLink.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <GithubLink />
-
-  <header>
-    <h1>BSN-tool</h1>
-  </header>
+  <Header />
 
   <main>
     <form autocomplete="off" onsubmit="return false;">
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>BSN-tool</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  margin-top: 5vh;
+  background: #666;
+  background: linear-gradient(to right, #666 0%, #b8b8b8 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
