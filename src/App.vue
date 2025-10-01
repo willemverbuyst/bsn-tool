f8efe0c25d9ec09bc0abebbdfdd979da731e55c3
Willem Verbuyst <willemverbuyst@gmail.com>
2025-10-01 06:57:49 +0200

    Separate link into component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,11 @@
 import BsnGeneratorButton from "./components/BsnGeneratorButton.vue";
 import BsnInput from "./components/BsnInput.vue";
 import BsnValidatorButton from "./components/BsnValidatorButton.vue";
+import GithubLink from "./components/GithubLink.vue";
 </script>
 
 <template>
-  <section class="link">
-    <a
-      href="https://github.com/willemverbuyst/bsn-tool"
-      target="_blank"
-      rel="noreferrer"
-      aria-labelledby="gh-icon"
-      ><i class="fab fa-github icon__github">GH ICON</i></a
-    >
-  </section>
+  <GithubLink />
 
   <header>
     <h1>BSN-tool</h1>
---
+++ src/components/GithubLink.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <section class="link">
+    <a
+      href="https://github.com/willemverbuyst/bsn-tool"
+      target="_blank"
+      rel="noreferrer"
+      aria-labelledby="gh-icon"
+      ><i class="fab fa-github icon__github">GH ICON</i></a
+    >
+  </section>
+</template>
+
+<style scoped>
+.link {
+  display: flex;
+  justify-content: end;
+  padding: 1rem;
+
+  .icon__github {
+    background-image: linear-gradient(to right top, #cbced1, #044d8c);
+    background-clip: text;
+    color: transparent;
+    font-size: 4rem;
+  }
+}
+</style>
