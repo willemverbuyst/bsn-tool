e22946f8a6162a9830df4a44eb0512b0ed2e42ee
Willem Verbuyst <willemverbuyst@gmail.com>
2025-10-18 04:19:12 +0200

    Update base path in Vite configuration to '/bsn-tool/'

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: " / ",
+  base: " /bsn-tool/ ",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
